8430dbae45e06c75d5b1b837fd4a7004bc8bad65
rubblesky <2018211896@mail.hfut.edu.cn>
2020-08-26 11:29:17 +0800

    add get_cmd faction

--- src/piPlayer.c
+++ src/piPlayer.c
@@ -5,17 +5,18 @@
 #include <dirent.h>
 #include <errno.h>
 #include <getopt.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
 #include <unistd.h>
 
 #include "dir.h"
+#include "err.h"
 #include "sort.h"
 #include "terminal.h"
-#include "err.h"
-
 
 //extern char **environ;
 
@@ -33,13 +34,14 @@ bool show_hidden_files = 0;
 
 /*绘制界面*/
 static int print_interface();
+/*读取键盘命令*/
+static void *get_cmd(void *arg);
 
 /*打印目录*/
 static void print_dir();
 
 /*初始化目录*/
 static char *init_directory_name(char *directory);
-
 
 static char short_opts[] = "std:";
 static struct option long_opt[] =
@@ -76,6 +78,13 @@ int main(int argc, char *argv[]) {
                 break;
         }
     }
+    pthread_t tidp;
+    if (pthread_create(&tidp, NULL, get_cmd, NULL) < 0) {
+#ifndef NDEBUG
+        printf("can't create control pthread\n");
+#endif
+        return -1;
+    }
     print_interface();
 }
 
@@ -90,19 +99,63 @@ int print_interface() {
         return -1;
     }
 
-
 #ifndef NDEBUG
     /*打印排序结果*/
     if (play_list.sort_rule == by_last_modification_time)
         for (int i = 0; i < play_list.file_used_num; i++) {
             printf("%d: %25s  time: %d\n", i + 1, play_list.sorted_file[i]->name, play_list.sorted_file[i]->stat.st_mtime);
         }
-    else if(play_list.sort_rule == by_name)
-        for (int i = 0; i < play_list.file_used_num;i++){
+    else if (play_list.sort_rule == by_name)
+        for (int i = 0; i < play_list.file_used_num; i++) {
             printf("%d: %s \n", i + 1, play_list.sorted_file[i]->name);
         }
 #endif
     play_list.current_music = 0;
+    while (1) {
+    }
+}
+
+static void *get_cmd(void *arg) {
+    char c;
+    /*
+    if (setvbuf(stdin,NULL,_IONBF,0) != 0) {
+#ifndef NDEBUG
+        printf("buf modify error\n");
+#endif
+
+    }
+*/
+    struct termios old;
+    if (tcgetattr(fileno(stdin), &old) < 0) {
+#ifndef NDEBUG
+        printf("can't get tty attribute\n");
+#endif
+        exit(-1);
+    }
+    /*修改模式为非规范模式*/
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    if (tcsetattr(fileno(stdin), TCSADRAIN, &old) < 0) {
+#ifndef NDEBUG
+        printf("can't set tty attribute\n");
+#endif
+        exit(-1);
+    }
+
+    while ((c = fgetc(stdin)) != EOF) {
+        switch (c) {
+            case 'u':
+                play_list.current_music--;
+                break;
+            case 'd':
+                play_list.current_music++;
+                break;
+            case 'q':
+                exit(0);
+            default:
+                break;
+        }
+    }
 }
 
 static void print_dir() {
@@ -110,8 +163,6 @@ static void print_dir() {
         printf("%s\n", play_list.file->name);
     }
 }
-
-
 
 static char *init_directory_name(char *directory) {
     int size = strlen(directory);
